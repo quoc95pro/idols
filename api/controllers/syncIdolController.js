c041c4fdd758aeb93bdaccfc5338abc5294657a6
quoc95pro <quocvcd00047@fpt.edu.vn>
2018-05-04 07:34:09 +0700

    ok

--- api/controllers/syncIdolController.js
+++ api/controllers/syncIdolController.js
@@ -2,6 +2,8 @@ var Idol = require("../models/idolModel");
 const https = require("https");
 var config = require("../../config");
 var async = require("async");
+
+var request = require('request');
 function getAllIdol(callback){
     Idol.find(function(err, idols){
         if(err){
@@ -26,6 +28,46 @@ function checkIdolExist(name, callback){
         }
     });
 }
+
+let addPr = (a, b) => {
+   return new Promise((resolve, reject) => {
+       setTimeout(() => {
+           if(typeof a!= "number" || typeof b != "number"){
+                return reject(new Error ('Tham so phai la kieu number'));
+           }
+           resolve(a + b);
+       }, 2000);
+   });
+}
+
+let add = async (callback) => {
+    let rs = await addPr(4,5);
+    callback(rs);
+}
+
+let requestVideoPr = (apiId) => {
+    return new Promise((resolve, reject) => {
+
+            request(config.getFileFromOpenload()+apiId, function (error, response, body) {
+           
+                //console.log('error:', error); 
+                console.log(apiId);
+                console.log('statusCode:', response && response.statusCode); 
+                //body = JSON.parse(body);
+                //console.log('body:', body);
+                
+                    resolve(response.statusCode);
+                
+            });  
+       
+    });
+ }
+
+ let requestVideo = async (apiId, callback) => {
+    let rs = await requestVideoPr(apiId);
+    callback(rs);
+}
+
 
 module.exports = function(app){
     // sync from openload
@@ -67,50 +109,43 @@ module.exports = function(app){
     // sync file from openload 
     app.get("/api/syncFile",  (req, res) => {
         getAllIdol(function(ok){
-            async.forEachOf(ok, (value, key, callback) => {
-                   
-                https.get(config.getFileFromOpenload(), resp => {
-                    resp.setEncoding("utf8");
-                    let body = "";
-                    resp.on("data", data => {
-                        body += data;
-                    });
-                    
-                    resp.on("end", () => {
-                        var seedIdol = [];
-                        body = JSON.parse(body);
-                       
-                        async.forEachOf(body.result.folders, (value, key, callback) => {
-                           
-                                checkIdolExist(value.name, function(name){
-                                    if(name){
-                                        seedIdol.push({
-                                            name: value.name,
-                                            apiFolderId: value.id
-                                        });
-                                    }
-                                    callback();
+
+            // async.forEachOf(ok, (value, key, callback) => {
+            //     setTimeout(function () {
+            //         requestVideo(value.apiFolderId,(rs)=>{
+            //                 //console.log(rs);
+            //                 callback();
+            //         });
+            //     },10000);
+            // }, err => {
+            //     if (err) console.error(err.message);
+            //     res.send('ok');
+            // });
+                
+            async.eachSeries(ok, function (eachUrl, done) {
+                setTimeout(function () {
+                    requestVideo(eachUrl.apiFolderId,(rs)=>{
+                                        //console.log(rs);
+                                        done();
                                 });
-                            
-                        }, err => {
-                            if (err) console.error(err.message);
-                              Idol.create(seedIdol, function(err, results){ 
-                                 res.send(results);
-                             });
-                        });
-        
-                     });
-                     
-                });
-            //
-            }, err => {
+                }, 100);
+            }, function (err) {
                 if (err) console.error(err.message);
-                Idol.create(seedIdol, function(err, results){ 
-                    res.send(results);
-                });
+                res.send(ok);
             });
+
         });
     });
 
-
+    app.get("/api/test", (req, res) => {
+        request(config.getFileFromOpenload()+'5186505', function (error, response, body) {
+           
+                //console.log('error:', error); 
+                
+                console.log('statusCode:', response && response.statusCode); 
+                body = JSON.parse(body);
+                console.log('body:', body);
+               
+            });  
+    });
 }
