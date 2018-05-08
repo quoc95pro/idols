99fa18265e30f1b0dd4fb27735712033a707c3f0
Quoc Vu Cuong  (FIS FPS HN) <QuocVC@fpt.com.vn>
2018-05-08 16:30:45 +0700

    8/5/2018

--- api/controllers/idolController.js
+++ api/controllers/idolController.js
@@ -36,5 +36,59 @@ module.exports = function(app){
             }
         });
     });
+
+    // update idol by Id
+    app.put("/api/idol", (req, res) =>{
+        if(!req.body._id){
+            return res.status(500).send("ID is required");
+        }else{
+            Idol.update({_id : req.body._id},{
+                name: req.body.name,
+                apiFolderId: req.body.apiFolderId,
+                dob : req.body.dob,
+                nationality : req.body.nationality,
+                height : req.body.height,
+                measurement : {
+                    bust : req.body.measurement.bust,
+                    waist : req.body.measurement.waist,
+                    hips : req.body.measurement.hips
+                }
+            }, (err, idol) =>{
+                if(err) return res.status(500).json(err);
+                res.send('updated');
+            })
+        }
+    });
+
+    // create idol
+    app.post("/api/idol", (req, res) => {
+        var idol ={
+            name: req.body.name,
+            apiFolderId: req.body.apiFolderId,
+            dob : req.body.dob,
+            nationality : req.body.nationality,
+            height : req.body.height,
+            measurement : {
+                bust : req.body.measurement.bust,
+                waist : req.body.measurement.waist,
+                hips : req.body.measurement.hips
+            }
+        };
+
+        Idol.create(idol, (err, idol) => {
+            if(err) res.status(500).json(err);
+            res.send('created');
+        });
+    });
+
+    // delete idol 
+    app.delete("/api/idol/:id", (req,res) =>{
+        Idol.remove({
+            _id : req.body._id
+        }, (err, idol) => {
+            if(err) res.status(500).json(err);
+            res.send('deleted');
+        })
+    })
    
 }    
--- api/models/idolModel.js
+++ api/models/idolModel.js
@@ -4,7 +4,15 @@ var Schema = mongoose.Schema;
 
 var idolSchema = new Schema({
     name: String,
-    apiFolderId: String
+    apiFolderId: {type : Number, default : null},
+    dob : {type : Date, default : null},
+    nationality : {type : String, default : 'Japan'},
+    height : {type : Number, default : null},
+    measurement : {
+        bust : {type : Number, default : null},
+        waist : {type : Number, default : null},
+        hips : {type : Number, default : null}
+    }
 });
 
 var Idol = mongoose.model("Idols", idolSchema);
