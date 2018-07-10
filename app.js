446975e3924e1aff0bf6b21a5fbda91a55a31323
quoc95pro <quocvcd00047@fpt.edu.vn>
2018-07-10 21:36:17 +0700

    ok

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ var config = require("./config");
 var app = express();
 var port = process.env.PORT || 3000;
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
+
+app.use(allowCrossDomain);
+ 
+
 app.use("/assets", express.static(__dirname + "public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
