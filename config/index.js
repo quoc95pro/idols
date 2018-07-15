c0954dea43029e192598ab7ffebbeaf2173c024c
quoc95pro <quocvcd00047@fpt.edu.vn>
2018-07-16 06:50:04 +0700

    ok

--- config/index.js
+++ config/index.js
@@ -2,7 +2,8 @@ var configValues = require("./config");
 
 module.exports = {
     getDBConnectionString: function(){
-        return 'mongodb://127.0.0.1:27017/idol';
+        // return 'mongodb://127.0.0.1:27017/idol';
+        return `mongodb://${configValues.username}:${configValues.password}@ds235401.mlab.com:35401/idol`;
     },
     getIdolFromOpenload: function(){
         return 'https://api.openload.co/1/file/listfolder?login=6d0fa8888208b015&key=CHIhWOYv';
